9acfdada72794344145d90da0feafaa1d74df5a0
TheAmlanSaikia <saikiaamlanj@gmail.com>
2021-06-02 00:51:32 +0530

    Until Exercise 4

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var txtOutput = document.querySelector("txt-output");
 function clickEventListner(){
-    console.log("Click!")
+    console.log("clicked")
+    console.log("Input=",txtInput.value);
+    
+    
 }
 
 btnTranslate.addEventListener("click",clickEventListner);
