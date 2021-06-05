01a2e410697a9e130310f32b5580cdcba1d9bbd5
TheAmlanSaikia <saikiaamlanj@gmail.com>
2021-06-05 11:39:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
-var txtOutput = document.querySelector("txt-output");
+var txtOutput = document.querySelector("#txt-output");
 function clickEventListner(){
     console.log("clicked")
     console.log("Input=",txtInput.value);
     
+    txtOutput.innerText = txtInput.value;
+  
     
 }
 
