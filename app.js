82bc4db672e6cf6b286e695ffa2ed6fdbbeea2df
TheAmlanSaikia <saikiaamlanj@gmail.com>
2021-06-06 02:57:39 +0530

    Update app.js till exercise

--- app.js
+++ app.js
@@ -11,3 +11,30 @@ function clickEventListner(){
 }
 
 btnTranslate.addEventListener("click",clickEventListner);
+
+
+var text1 = "I am Ironman";
+var text2 = "I am Batman";
+var text3 = "I am Shakitiman";
+
+
+
+function urlAdder(text){
+
+  
+  return "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"+"?"+"text="+text;
+}
+
+function doFetch(text){
+  fetch(urlAdder(text))
+  .then(response=>response.json())
+  .then(json=>console.log(json))
+}
+
+doFetch(text1);
+doFetch(text2);
+doFetch(text3);
+
+
+
+
